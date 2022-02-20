5d655cf186301e31add3c04f374824983a96c2b6
Simon Fink <simon.2.fink@gmail.com>
2022-02-20 13:18:55 +0100

    Add font awesome packages

--- client/funkers/src/main.ts
+++ client/funkers/src/main.ts
@@ -4,9 +4,17 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
+library.add(faPhone);
+
 const app = createApp(App)
 
 app.use(createPinia())
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router)
 
 app.mount('#app')

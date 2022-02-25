9ce17868d1bdc8afeeea2a27827c606a242ab7b3
Simon Fink <simon.2.fink@gmail.com>
2022-02-25 16:35:47 +0100

    Use Quasar with icon libraries

--- client/funkers/src/main.ts
+++ client/funkers/src/main.ts
@@ -1,18 +1,23 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { Quasar } from 'quasar'
 
 import App from './App.vue'
 import router from './router'
 
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faAngleDoubleLeft, faHome, faInfoCircle, faUtensils } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add( faAngleDoubleLeft, faHome, faInfoCircle, faUtensils)
-
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/mdi-v6/mdi-v6.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import '@quasar/extras/ionicons-v4/ionicons-v4.css'
+import '@quasar/extras/line-awesome/line-awesome.css'
+import '@quasar/extras/bootstrap-icons/bootstrap-icons.css'
 
 createApp(App)
     .use(createPinia())
     .use(router)
-    .component('FontAwesomeIcon', FontAwesomeIcon)
+    .use(Quasar)
     .mount('#app')
